4ae9394e3f5b39398be9fbe788f57c5919863db7
Yevgeniy Shek <shek.yujin@gmail.com>
2023-12-07 00:38:05 +0600

    feature: add BaseRadioButton

---
+++ src/components/shared/BaseRadioButton.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+defineProps<{
+  label: string;
+  value: string | number;
+  modelValue: string | number;
+}>();
+
+const emit = defineEmits<{
+  'update:modelValue': [value: string | number];
+}>();
+
+const updateInput = (e: Event) => {
+  emit('update:modelValue', (e.target as HTMLInputElement).value);
+};
+</script>
+
+<template>
+  <label class="inline-block">
+    <input
+      class="accent-brand-500"
+      type="radio"
+      :checked="modelValue === value"
+      :value="value"
+      @change="updateInput"
+    />
+    <span class="ml-1">{{ label }}</span>
+  </label>
+</template>
