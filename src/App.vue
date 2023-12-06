8094fbfcd70a860ab867795fe2e705d5e0d7c2e2
Yevgeniy Shek <shek.yujin@gmail.com>
2023-12-07 00:43:58 +0600

    feature: update main layout

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <script setup lang="ts"></script>
 
-<template><h1>App</h1></template>
+<template>
+  <div class="container mx-auto p-6 md:p-8">
+    <header class="flex items-center justify-between border-b-2 py-3">
+      <router-link class="text-3xl font-bold text-brand-500" to="/">Todo App</router-link>
+      <a href="https://github.com/shekeg/todo-app" target="_blank" rel="noopener noreferrer">
+        <img src="@/assets/icons/github.svg" width="40" height="40" alt="GitHub repo" />
+      </a>
+    </header>
+    <main class="py-7">
+      <router-view />
+    </main>
+  </div>
+</template>
