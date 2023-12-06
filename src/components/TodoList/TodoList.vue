36d0d95984053cb05df9c490ef458f8ab2038a27
Yevgeniy Shek <shek.yujin@gmail.com>
2023-12-07 00:42:44 +0600

    feature: add TodoList

---
+++ src/components/TodoList/TodoList.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import TodoListItem from './TodoListItem.vue';
+</script>
+
+<template>
+  <section>
+    <ul>
+      <TodoListItem v-for="i in 30" :id="i" :key="i" class="mt-5 first:mt-0" />
+    </ul>
+  </section>
+</template>
---
+++ src/components/TodoList/TodoListItem.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import BaseButton from '@/components/shared/BaseButton.vue';
+import BaseRouterLinkButton from '@/components/shared/BaseRouterLinkButton.vue';
+</script>
+
+<template>
+  <li
+    class="flex flex-col rounded-2xl bg-gray-100 p-6 md:flex-row md:items-center md:justify-between"
+  >
+    <p>Design use case page</p>
+    <div class="mt-4 flex gap-4 md:ml-4 md:mt-0">
+      <BaseRouterLinkButton
+        :to="{ name: 'TodoUpdatePage', params: { id: 1 } }"
+        intent="primary"
+        class="flex-1"
+      >
+        Edit
+      </BaseRouterLinkButton>
+      <BaseButton intent="distractive" class="flex-1">Delete</BaseButton>
+    </div>
+  </li>
+</template>
