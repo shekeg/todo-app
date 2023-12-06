949a17407865c09e98226c6589ec0d9e08f97579
Yevgeniy Shek <shek.yujin@gmail.com>
2023-12-07 00:38:44 +0600

    feature: add BaseTextarea

---
+++ src/components/shared/BaseTextarea.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { defineProps, defineEmits } from 'vue';
+
+defineProps<{
+  modelValue: string;
+}>();
+
+const emit = defineEmits<{
+  'update:modelValue': [value: string];
+}>();
+
+const updateInput = (e: Event) => {
+  emit('update:modelValue', (e.target as HTMLTextAreaElement).value);
+};
+</script>
+
+<template>
+  <textarea
+    class="inline-block rounded-xl bg-gray-100 px-3 py-2"
+    rows="5"
+    :value="modelValue"
+    @input="updateInput"
+  >
+  </textarea>
+</template>
