e8775156c25706d4dd3443cab2fdf793a05d20f1
Yevgeniy Shek <shek.yujin@gmail.com>
2023-12-07 00:37:15 +0600

    feature: add BaseCheckbox

---
+++ src/components/shared/BaseCheckbox.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+defineProps<{
+  label: string;
+  modelValue: boolean;
+}>();
+
+const emit = defineEmits<{
+  'update:modelValue': [value: boolean];
+}>();
+
+const updateInput = (e: Event) => {
+  emit('update:modelValue', (e.target as HTMLInputElement).checked);
+};
+</script>
+
+<template>
+  <label class="inline-block">
+    <input class="accent-brand-500" type="checkbox" :checked="modelValue" @change="updateInput" />
+    <span class="ml-1">{{ label }}</span>
+  </label>
+</template>
