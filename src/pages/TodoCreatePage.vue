d4cade1c2c9e9758cb00491f2e4afeb02e3a5645
Yevgeniy Shek <shek.yujin@gmail.com>
2023-12-07 16:47:15 +0600

    feature: add data binding for TodoCreatePage

--- src/pages/TodoCreatePage.vue
+++ src/pages/TodoCreatePage.vue
@@ -1,27 +1,60 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 import BaseCheckbox from '@/components/shared/BaseCheckbox.vue';
 import BaseTextarea from '@/components/shared/BaseTextarea.vue';
 import BaseButton from '@/components/shared/BaseButton.vue';
 
-const done = ref(false);
-const description = ref('');
+import { useToastNotifications } from '@/composables/useToastNotifications.ts';
+import { useTodoData } from '@/composables/useTodoData.ts';
+
+const router = useRouter();
+const $toast = useToastNotifications();
+const todoData = useTodoData();
+
+const isCompleted = ref(false);
+const todo = ref('');
+
+const handleSubmit = () => {
+  const payload = {
+    completed: isCompleted.value,
+    todo: todo.value,
+  };
+  todoData.create(payload).then(() => {
+    $toast.success('Created successfully');
+    router.back();
+  });
+};
 </script>
 
 <template>
   <h1 class="text-xl font-bold">Create new task</h1>
-  <BaseCheckbox v-model="done" class="mt-7" label="Completed" />
-  <BaseTextarea
-    v-model="description"
-    class="mt-4 w-full"
-    placeholder="Add a note..."
-    aria-label="Add a note..."
-  />
-  <div class="mt-4 flex gap-4">
-    <BaseButton class="ml-auto flex-1 md:flex-none" intent="primary">Create</BaseButton>
-    <BaseButton class="flex-1 md:flex-none" intent="distractive" @click="() => $router.back()">
-      Cancel
-    </BaseButton>
-  </div>
+  <form @submit.prevent="handleSubmit">
+    <BaseCheckbox v-model="isCompleted" class="mt-7" label="Completed" />
+    <BaseTextarea
+      v-model="todo"
+      class="mt-4 w-full"
+      placeholder="Add a note..."
+      aria-label="Add a note..."
+    />
+    <div class="mt-4 flex gap-4">
+      <BaseButton
+        class="ml-auto flex-1 md:flex-none"
+        intent="primary"
+        type="submit"
+        :disabled="todo.length === 0"
+      >
+        Create
+      </BaseButton>
+      <BaseButton
+        class="flex-1 md:flex-none"
+        intent="distractive"
+        type="button"
+        @click="() => $router.back()"
+      >
+        Cancel
+      </BaseButton>
+    </div>
+  </form>
 </template>
