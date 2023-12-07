9495e23dc4bc996f7d25e0009fcb427826fa3c6b
Yevgeniy Shek <shek.yujin@gmail.com>
2023-12-07 16:46:32 +0600

    feature: add data binding for TodoFilters

--- src/components/TodoFilters/TodoFilters.vue
+++ src/components/TodoFilters/TodoFilters.vue
@@ -1,18 +1,47 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import BaseInput from '@/components/shared/BaseInput.vue';
 import BaseRadioButton from '@/components/shared/BaseRadioButton.vue';
+import { STATUS_OPTIONS } from '@/composables/useTodoFilters';
 
-const statusFilter = ref('all');
+const props = defineProps<{
+  name: string;
+  status: string;
+}>();
+
+const emit = defineEmits<{
+  'update:name': [name: string];
+  'update:status': [status: string];
+}>();
 </script>
 
 <template>
   <div>
-    <BaseInput class="w-full" placeholder="Search..." aria-label="Search" />
+    <BaseInput
+      class="w-full"
+      placeholder="Search..."
+      aria-label="Search"
+      :value="props.name"
+      @input="emit('update:name', $event.target.value)"
+    />
     <div class="mt-4 flex flex-col gap-3 md:flex-row md:gap-5">
-      <BaseRadioButton id="all" v-model="statusFilter" label="All" value="all" />
-      <BaseRadioButton id="active" v-model="statusFilter" label="Active" value="active" />
-      <BaseRadioButton id="completed" v-model="statusFilter" label="Completed" value="completed" />
+      <BaseRadioButton
+        :checked="props.status === STATUS_OPTIONS.all"
+        :value="STATUS_OPTIONS.all"
+        label="All"
+        @change="emit('update:status', STATUS_OPTIONS.all)"
+      />
+      <BaseRadioButton
+        :checked="props.status === STATUS_OPTIONS.active"
+        :value="STATUS_OPTIONS.active"
+        label="Active"
+        @change="emit('update:status', STATUS_OPTIONS.active)"
+      />
+      <BaseRadioButton
+        :checked="props.status === STATUS_OPTIONS.completed"
+        :value="STATUS_OPTIONS.completed"
+        label="Completed"
+        @change="emit('update:status', STATUS_OPTIONS.completed)"
+      />
     </div>
   </div>
 </template>
