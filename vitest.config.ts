5bfaf54a2a1921d95a664c1cd5a1ed3317d0e4bb
Yevgeniy Shek <shek.yujin@gmail.com>
2023-12-08 03:52:38 +0600

    chore: configure vitest to work with Vue

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [vue(), tsconfigPaths()],
   test: {
+    alias: {
+      '@/': new URL('./src/', import.meta.url).pathname,
+    },
     coverage: {
       provider: 'v8',
+      include: ['src/**/*.{js,ts,vue}', '!src/**/types.ts'],
     },
     restoreMocks: true,
     globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./vitest.setup.ts'],
   },
 });
---
+++ vitest.setup.ts
@@ -0,0 +1,3 @@
+import { config, RouterLinkStub } from '@vue/test-utils';
+
+config.global.stubs['router-link'] = RouterLinkStub;
