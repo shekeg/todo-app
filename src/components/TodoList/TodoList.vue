246dc061ff6de711393f6f941aa7fa7f6dedcdac
Yevgeniy Shek <shek.yujin@gmail.com>
2023-12-07 16:46:20 +0600

    feature: add data binding for TodoList

--- src/components/TodoList/TodoList.vue
+++ src/components/TodoList/TodoList.vue
@@ -1,11 +1,27 @@
 <script lang="ts" setup>
 import TodoListItem from './TodoListItem.vue';
+import { type TodoDataItem } from '@/composables/useTodoData';
+
+defineProps<{
+  todoItems: TodoDataItem[];
+}>();
 </script>
 
 <template>
   <section>
-    <ul>
-      <TodoListItem v-for="i in 30" :id="i" :key="i" class="mt-5 first:mt-0" />
+    <template v-if="todoItems.length === 0">
+      <p class="text-center">No results.</p>
+      <p class="text-center">Check filter params or reset LocalStorage to refetch todos.</p>
+    </template>
+    <ul v-else>
+      <TodoListItem
+        v-for="todoItem in todoItems"
+        :id="todoItem.id"
+        :key="todoItem.id"
+        :todo="todoItem.todo"
+        :completed="todoItem.completed"
+        class="mt-5 first:mt-0"
+      />
     </ul>
   </section>
 </template>
--- src/components/TodoList/TodoListItem.vue
+++ src/components/TodoList/TodoListItem.vue
@@ -1,22 +1,43 @@
 <script lang="ts" setup>
 import BaseButton from '@/components/shared/BaseButton.vue';
 import BaseRouterLinkButton from '@/components/shared/BaseRouterLinkButton.vue';
+
+import { useTodoData } from '@/composables/useTodoData.ts';
+import { useToastNotifications } from '@/composables/useToastNotifications';
+
+const props = defineProps<{
+  id: number;
+  todo: string;
+  completed: boolean;
+}>();
+
+const $toast = useToastNotifications();
+
+const todoData = useTodoData();
+
+const handleDeleteClick = () => {
+  todoData.remove(props.id).then(() => {
+    $toast.success('Deleted successfully');
+  });
+};
 </script>
 
 <template>
   <li
     class="flex flex-col rounded-2xl bg-gray-100 p-6 md:flex-row md:items-center md:justify-between"
   >
-    <p>Design use case page</p>
+    <p :class="{ 'line-through': completed }">{{ todo }}</p>
     <div class="mt-4 flex gap-4 md:ml-4 md:mt-0">
       <BaseRouterLinkButton
-        :to="{ name: 'TodoUpdatePage', params: { id: 1 } }"
+        :to="{ name: 'TodoUpdatePage', params: { id } }"
         intent="primary"
         class="flex-1"
       >
         Edit
       </BaseRouterLinkButton>
-      <BaseButton intent="distractive" class="flex-1">Delete</BaseButton>
+      <BaseButton intent="distractive" class="flex-1" @click="handleDeleteClick">
+        Delete
+      </BaseButton>
     </div>
   </li>
 </template>
