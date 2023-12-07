bcbd458701ca6bbca99a9fcbc226194c6079fd74
Yevgeniy Shek <shek.yujin@gmail.com>
2023-12-07 16:45:00 +0600

    feature: add useTodoFilters

---
+++ src/composables/useTodoFilters.ts
@@ -0,0 +1,45 @@
+import { useRouteQuery } from '@vueuse/router';
+import { computed, Ref } from 'vue';
+import { type TodoDataItem } from '@/composables/useTodoData.ts';
+
+const ROUTE_QUERY_KEYS = {
+  name: 'name',
+  status: 'status',
+};
+
+export const STATUS_OPTIONS = {
+  all: 'all',
+  active: 'active',
+  completed: 'completed',
+} as const;
+
+type StatusOptions = (typeof STATUS_OPTIONS)[keyof typeof STATUS_OPTIONS];
+
+export const useTodoFilters = (todos: Ref<TodoDataItem[]>) => {
+  const nameQuery = useRouteQuery<string>(ROUTE_QUERY_KEYS.name, '');
+  const statusQuery = useRouteQuery<StatusOptions>(ROUTE_QUERY_KEYS.status, STATUS_OPTIONS.all);
+
+  const filteredTodos = computed(() => {
+    if (todos.value === null) {
+      return [];
+    }
+    const filteredByName = todos.value.filter((t) => t.todo.includes(nameQuery.value || ''));
+
+    switch (statusQuery.value) {
+      case STATUS_OPTIONS.all:
+        return filteredByName;
+      case STATUS_OPTIONS.completed:
+        return filteredByName.filter((t) => t.completed);
+      case STATUS_OPTIONS.active:
+        return filteredByName.filter((t) => !t.completed);
+      default:
+        return filteredByName;
+    }
+  });
+
+  return {
+    nameQuery,
+    statusQuery,
+    filteredTodos,
+  };
+};
