32ca91c0e88fb3763d2efc480f7f95610eb8fdf6
Yevgeniy Shek <shek.yujin@gmail.com>
2023-12-07 16:41:56 +0600

    feature: make BaseRadioButton controllable without v-model syntax

--- src/components/shared/BaseRadioButton.vue
+++ src/components/shared/BaseRadioButton.vue
@@ -2,15 +2,18 @@
 defineProps<{
   label: string;
   value: string | number;
-  modelValue: string | number;
+  modelValue?: string | number;
+  checked?: boolean;
 }>();
 
 const emit = defineEmits<{
   'update:modelValue': [value: string | number];
+  change: [e: Event];
 }>();
 
 const updateInput = (e: Event) => {
   emit('update:modelValue', (e.target as HTMLInputElement).value);
+  emit('change', e);
 };
 </script>
 
@@ -19,7 +22,7 @@ const updateInput = (e: Event) => {
     <input
       class="accent-brand-500"
       type="radio"
-      :checked="modelValue === value"
+      :checked="modelValue === value || checked"
       :value="value"
       @change="updateInput"
     />
